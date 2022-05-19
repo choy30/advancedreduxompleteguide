d4d64dafab3cec820c54a7f469f1a9680fb8fd77
devYeonjun <alcls0026@gmail.com>
2022-05-19 21:45:04 +1200

    Add useeffect with redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,15 @@ import Products from "./components/Shop/Products";
 
 function App() {
 	const showChart = useSelector((state) => state.ui.cartIsVisible);
+	const cart = useSelector((state) => state.cart);
+
+	useEffect(() => {
+		fetch("https://react-http-a1d35-default-rtdb.firebaseio.com/cart.json", {
+			method: "PUT",
+			body: JSON.stringify(cart),
+		});
+	}, [cart]);
+
 	return (
 		<Layout>
 			{showChart && <Cart />}
