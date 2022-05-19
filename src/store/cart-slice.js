8467aa147b5fd33dd4bebd3531d5a498c0433b67
devYeonjun <alcls0026@gmail.com>
2022-05-19 22:29:59 +1200

    Add fetching data

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import { uiActions } from "./ui-slice";
+
 
 const cartSlice = createSlice({
 	name: "cart",
@@ -9,6 +9,10 @@ const cartSlice = createSlice({
 		totalQuantity: 0,
 	},
 	reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+          },
 		addItemToCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -40,51 +44,6 @@ const cartSlice = createSlice({
 	},
 });
 
-export const sendCartData = (cart) => {
-	return async (dispatch) => {
-		dispatch(
-			uiActions.showNotification({
-				status: "pending",
-				title: "Sending...",
-				message: "Sending cart data",
-			})
-		);
-
-		const sendRequest = async () => {
-			const response = await fetch(
-				"https://react-http-a1d35-default-rtdb.firebaseio.com/cart.json",
-				{
-					method: "PUT",
-					body: JSON.stringify(cart),
-				}
-			);
-
-			if (!response.ok) {
-				throw new Error("Sending cart data failed");
-			}
-		};
-
-		try {
-			await sendRequest();
-
-			dispatch(
-				uiActions.showNotification({
-					status: "success",
-					title: "Success",
-					message: "Successfully sent cart data",
-				})
-			);
-		} catch (error) {
-			dispatch(
-				uiActions.showNotification({
-					status: "error",
-					title: "Error",
-					message: "Failed to send cart data",
-				})
-			);
-		}
-	};
-};
 
 export const cartActions = cartSlice.actions;
 
